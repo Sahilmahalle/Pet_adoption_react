366ae2791e2edd099a2f1b96a79e859b6fe76c0a
Sahilmahalle <sahilvmahalle@gmail.com>
2024-01-11 15:24:48 +0530

    Error boundaries added

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCache(error, info) {
+    //typically you would log this to something like track js or NewRelic
+    console.error("errorBoundary component cought an error", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h2>
+          There was an error with this listing .{" "}
+          <Link to="/">Click here to go back to the home page .</Link>;
+        </h2>
+      );
+    }
+    return this.props.children;
+  }
+}
+export default ErrorBoundary;
